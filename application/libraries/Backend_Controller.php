7caa31a5f8d653fcf01fa60b37e5d53ace58fd15
pakistanihaider <pakistanihaider@gmail.com>
2013-07-20 03:10:01 +0500

    First commit

---
+++ application/libraries/Backend_Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pakistanihaider
+ * Date: 20/07/13
+ * Time: 03:07
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Backend_Controller extends MY_Controller
+{
+    public function __construct(){
+        parent::__construct();
+    }
+}
---
+++ application/libraries/Frontend_Controller.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pakistanihaider
+ * Date: 20/07/13
+ * Time: 03:08
+ * To change this template use File | Settings | File Templates.
+ */
